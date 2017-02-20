76a38eca0a334653d8c229de2bdf387529b1e88e
Alex Smith <altaudiolabs@gmail.com>
2017-02-20 17:23:45 +0000

    Modified randomCat function to use defualts rather than || operator

--- functions.js
+++ functions.js
@@ -1,17 +1,20 @@
-function randomCat (name, breed, size, temperament) {
+function randomCat (options = {}) {
+
   let names = ["Pickles", "Tiger", "Sombrero", "Vic", "Smudge"];
   let breeds = ["British Shorthair", "Siamese", "Maine Coone", "Ragdoll", "Bengal"];
   let size1 = ["Small", "Medium", "Large"];
   let temperaments = ["Friendly", "Wary", "Active", "Playful", "Lazy"];
 
-  let catName = name || names[_.random(0, names.length - 1)];
-  let catBreed = breed || breeds[_.random(0, breeds.length - 1)];
-  let catSize = size || size1[_.random(0, size1.length - 1)];
-  let catTemperament = temperament || temperaments[_.random(0, temperaments.length - 1)];
-
-  return {'name' : catName, 'breed' : catBreed, 'size' : catSize, 'temperament' : catTemperament };
+  let defaults = {
+    'name' : names[_.random(0, names.length - 1)],
+    'breed' : breeds[_.random(0, breeds.length - 1)],
+    'size' : size1[_.random(0, size1.length - 1)],
+    'temperament' : temperaments[_.random(0, temperaments.length - 1)]
 }
 
+  let resultingCat = Object.assign({}, defaults, options);
+  return resultingCat;
+}
 
 export {randomCat};
 
